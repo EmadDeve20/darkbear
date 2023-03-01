c5dee32c0e44ec439e2c83746bb5e63495d72a08
EmadDeve20 <EmadDeve20@gmail.com>
2023-03-01 10:10:26 +0330

    improve code and add the get_server_msg function

--- darkbear/test_models.py
+++ darkbear/test_models.py
@@ -8,34 +8,42 @@ commands = [
 class CommandSender:
     def __init__(self, ssh_client: SSHClient) -> None:
         self.ssh_client = ssh_client
+        transport = self.ssh_client.get_transport()
+        self.channel = transport.open_session()
+        self.channel.get_pty()
+        self.channel.invoke_shell()
+        
+        print("\nTHE SERVER MESSAGE: ")
+        print(self.get_server_msg)
         print("\nSERVER NAME:")
         print(self.get_server_name)
+
+    @property
+    def get_server_msg(self) -> str:
+        """
+        get the login message from the server
+        return: messgae
+        """
+
+        return self.channel.recv(-1).decode()
+
 
     @property
     def get_server_name(self) -> str:
         """
         get the name of server
-        return: server_name: str
+        return: server_name
         """
 
-        transport = self.ssh_client.get_transport()
-        channel = transport.open_session()
-        channel.get_pty()
-        channel.invoke_shell()
-
-        channel.send("uname -a")
-        return channel.recv(-1).decode()
+        self.channel.send("uname -a")
+        return self.channel.recv(-1).decode()
 
     def run(self):
 
         for cmd in commands:
-            transport = self.ssh_client.get_transport()
-            channel = transport.open_session()
-            channel.get_pty()
-            channel.invoke_shell()
 
-            channel.send(cmd)
-            stdout = channel.recv(-1)
-            # print(f"stdin: {cmd}")
-            # print(f"stdout: {stdout.decode()}")
+            self.channel.send(cmd)
+            stdout = self.channel.recv(-1)
+            print(f"stdin: {cmd}")
+            print(f"stdout: {stdout.decode()}")
 
