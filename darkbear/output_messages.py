2eaed28d15caf96c6d840ce3d8ff4df5a995c766
EmadDeve20 <EmadDeve20@gmail.com>
2023-06-07 14:07:51 +0330

    add message for usb and mac address check

--- darkbear/output_messages.py
+++ darkbear/output_messages.py
@@ -4,4 +4,6 @@
 reports_types = {
     "package_manager_test": {"type": "suspicious", "description": "this server does not have a package manager! maybe this is a not real Linux like cowrie"},
     "chrooted_test": {"type": "very suspicious", "description": "this is very suspicious because the current directory is chrooted!"},
+    "network_macaddress_test": {"type": "very suspicious", "description": "The physical address of the server's network ports is for the virtual machine! The Server is a virtual machine!"},
+    "usb_virtual_machine_test": {"type": "suspicious", "description": "the some name of the USB device on the server has the word Virtual! so the server can be a Virtual Machine!"},
 }
